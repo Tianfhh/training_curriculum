d50afb35fdfa116099e2437f4c5ee71a94ca2a22
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-07-09 16:39:26 +0900

    Revert "Revert "Merge pull request #8 from Tianfhh/second_training""
    This reverts commit 7e9b57d0672dae1bc2c256ac19e51fd5a86211c9.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,8 +8,14 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
-    redirect_to action: :index
+    calendars_params = params.fetch(:calendars, {})
+    if calendars_params[:date].present? && calendars_params[:plan].present?
+      Plan.create(plan_params)
+      redirect_to action: :index
+    else
+      flash[:error] = "Missing or empty 'calendars' parameter"
+      redirect_to action: :index
+    end
   end
 
   private
@@ -34,7 +40,13 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = (@todays_date + x).wday
+      if wday_num >= 7
+        wday_num = wday_num -7
+      end
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday =>  "#{(@todays_date + x).month}/#{(@todays_date + x).day}#{wdays[wday_num]}" }
       @week_days.push(days)
     end
 
